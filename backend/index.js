d8a99c33b274cf4015cdab35494bc7621fae13ae
Leander Rodrigues <me@leander.xyz>
2021-02-04 21:06:14 -0500

    :sparkles: Introduce auth and login

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,23 @@
-const { Keystone } = require('@keystonejs/keystone');
-const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
-const { Text, Checkbox, Password } = require('@keystonejs/fields');
-const { GraphQLApp } = require('@keystonejs/app-graphql');
-const { AdminUIApp } = require('@keystonejs/app-admin-ui');
-const initialiseData = require('./initial-data');
+const { Keystone } = require("@keystonejs/keystone");
+const { PasswordAuthStrategy } = require("@keystonejs/auth-password");
+const { Text, Checkbox, Password } = require("@keystonejs/fields");
+const { GraphQLApp } = require("@keystonejs/app-graphql");
+const { AdminUIApp } = require("@keystonejs/app-admin-ui");
+const { NextApp } = require("@keystonejs/app-next");
+const initialiseData = require("./initial-data");
 
-const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
-const PROJECT_NAME = 'infravenous-backend';
-const adapterConfig = { mongoUri: 'mongodb://localhost/infravenous' };
-
+const { MongooseAdapter: Adapter } = require("@keystonejs/adapter-mongoose");
+const PROJECT_NAME = "infravenous-backend";
+const adapterConfig = { mongoUri: "mongodb://localhost/infravenous" };
 
 const keystone = new Keystone({
   adapter: new Adapter(adapterConfig),
-  onConnect: process.env.CREATE_TABLES !== 'true' && initialiseData,
+  onConnect: process.env.CREATE_TABLES !== "true" && initialiseData,
 });
 
 // Access control functions
-const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
+const userIsAdmin = ({ authentication: { item: user } }) =>
+  Boolean(user && user.isAdmin);
 const userOwnsItem = ({ authentication: { item: user } }) => {
   if (!user) {
     return false;
@@ -27,7 +28,7 @@ const userOwnsItem = ({ authentication: { item: user } }) => {
   return { id: user.id };
 };
 
-const userIsAdminOrOwner = auth => {
+const userIsAdminOrOwner = (auth) => {
   const isAdmin = access.userIsAdmin(auth);
   const isOwner = access.userOwnsItem(auth);
   return isAdmin ? isAdmin : isOwner;
@@ -35,7 +36,7 @@ const userIsAdminOrOwner = auth => {
 
 const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
 
-keystone.createList('User', {
+keystone.createList("User", {
   fields: {
     name: { type: Text },
     email: {
@@ -66,8 +67,8 @@ keystone.createList('User', {
 
 const authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
-  list: 'User',
-  config: { protectIdentities: process.env.NODE_ENV === 'production' },
+  list: "User",
+  config: { protectIdentities: process.env.NODE_ENV === "production" },
 });
 
 module.exports = {
@@ -76,8 +77,9 @@ module.exports = {
     new GraphQLApp(),
     new AdminUIApp({
       name: PROJECT_NAME,
-      enableDefaultRoute: true,
+      enableDefaultRoute: false,
       authStrategy,
     }),
+    new NextApp({ dir: "../web" }),
   ],
 };
---
+++ backend/keystone.ts
@@ -0,0 +1,52 @@
+import { User } from './schema/User'
+import { createAuth } from '@keystone-next/auth'
+import { config, createSchema } from '@keystone-next/keystone/schema'
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session'
+import 'dotenv/config'
+
+const databaseUrl =
+  process.env.DATABASE_URL || 'mongodb://localhost/infravenous'
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24,
+  secret: process.env.COOKIE_SECRET,
+}
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add roles here
+  },
+})
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL, process.env.MOBILE_APP],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseUrl,
+    },
+    lists: createSchema({
+      User,
+    }),
+    ui: {
+      // If the user is signed in
+      // TODO: Permit based on role
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+)
