ef42138874b8477d48b332044e85215397582a55
Leander Rodrigues <me@leander.xyz>
2021-03-20 16:19:23 -0400

    :sparkles: small edits

--- device/conductor.py
+++ device/conductor.py
@@ -2,14 +2,22 @@ import os
 import random
 from dotenv import load_dotenv
 from archivist import archive_attempt
-from analyst import authenticate
+
+# from analyst import authenticate
+from datetime import datetime
 
 # Load the environment variables
+print("Loading secure variables")
 load_dotenv()
+
+# Perform the authentication attempt
+# result = authenticate()
+# confidence = result.confidence;
 
 # Create the attempt information packet
 attempt = {
     "confidence": random.randint(60, 100),
+    # "confidence": int(confidence),
     "threshold": int(os.getenv("AUTHENTICATION_THRESHOLD")),
     "url": os.getenv("BACKEND_URL"),
     "user_id": os.getenv("USER_ID"),
@@ -17,4 +25,8 @@ attempt = {
 }
 
 # Archive it to the hosted database
+start_time = datetime.timestamp(datetime.now())
 archive_attempt(**attempt)
+print("Authentication attempt saved.")
+end_time = datetime.timestamp(datetime.now())
+print("Network Latency: " + str(round(end_time - start_time, 3)) + "ms")
